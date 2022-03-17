a599eb2bf80bc67a4f787ac14e506eaa73870df5
Peter <peter.palagyi@gmail.com>
2022-03-17 14:56:16 +0100

    auth page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
