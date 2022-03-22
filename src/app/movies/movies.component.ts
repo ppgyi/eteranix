62acbb8bb4c61e0fdbcf8885cfccd2706f3f5db0
Peter <peter.palagyi@gmail.com>
2022-03-22 16:00:44 +0100

    movie component - navBarDown

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-movies',
@@ -9,6 +9,8 @@ export class MoviesComponent implements OnInit {
 
   spanButton = "buttonSpanRight";
   navBar = "navBarLeftInvisible animate__animated animate__fadeInLeft";
+
+  @ViewChild('panel') panel!: ElementRef;
 
   constructor() { }
 
@@ -37,5 +39,12 @@ export class MoviesComponent implements OnInit {
       this.spanButton = "buttonSpanRight";
       this.navBar = "navBarLeftInvisible";
     }
+  }
+
+  onClickRight() {
+    this.panel.nativeElement.scrollLeft += 150;
+  }
+  onClickLeft() {
+    this.panel.nativeElement.scrollLeft -= 150;
   }
 }
