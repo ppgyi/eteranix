c928ba715026f1df07197d6aa42398ce6b10f09a
Peter <peter.palagyi@gmail.com>
2022-03-22 07:25:58 +0100

    colors change + movie component

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
+  spanButton = "buttonSpanRight";
+  navBar = "navBarLeftInvisible animate__animated animate__fadeInLeft";
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  spanButtonChange() {
+    if (this.spanButton == "buttonSpanRight"){
+      this.spanButton = "buttonSpanMiddle";
+      this.navBar = "navBarLeftVisible";
+    } else {
+      this.spanButton = "buttonSpanRight";
+      this.navBar = "navBarLeftInvisible";
+    }
+  }
+
+  navBarVisible() {
+    if (this.navBar == "navBarLeftInvisible" || this.navBar == "navBarLeftInvisible animate__animated animate__fadeInLeft"){
+      this.spanButton = "buttonSpanMiddle";
+      this.navBar = "navBarLeftVisible";
+    }
+  }
+
+  navBarInvisible() {
+    if (this.navBar == "navBarLeftVisible"){
+      this.spanButton = "buttonSpanRight";
+      this.navBar = "navBarLeftInvisible";
+    }
+  }
 }
